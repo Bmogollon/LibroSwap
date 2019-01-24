78e15fd36d0298489520538b01f42999938d9f88
Bonny Mogollon <bonny.mogollon11@gmail.com>
2019-01-24 12:15:35 -0500

    users table and users function

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, :uniqueness => { :case_sensitive => false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }, :on => :create
+    has_secure_password
+end
