1a127f461dc136714458712e4317c4e73327e929
Bonny Mogollon <bonny.mogollon11@gmail.com>
2019-02-01 00:24:05 -0500

    heroku

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -6,8 +6,8 @@ function createMessageChannel() {
          received: function(data) {
            $("#messages").removeClass('hidden')
            $("#message_content").val('')
-           window.location.reload();
            return $('#messages').append(this.renderMessage(data));
+           window.location.reload();
          },
          renderMessage: function(data) {
      return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
