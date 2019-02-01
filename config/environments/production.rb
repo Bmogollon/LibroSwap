8047ac1342f96383d47f02e289de93c56712adab
Bonny Mogollon <bonny.mogollon11@gmail.com>
2019-01-31 23:32:07 -0500

    chat reloader

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://libroswap.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://libroswap.herokuapp.com', 'http://libroswap.herokuapp.com']
+
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
